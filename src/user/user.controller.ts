6d8bf5db6159ddc4fe6ca2099f96676828b3e53b
Jill-Anne Bragado <jillannebragado13@gmail.com>
2025-07-14 03:40:44 +0800

    feat(user): enhance CreateUserDto with validation decorators(use pipes) and refactor UserController

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,18 @@
 import { CreateUserDto } from '@/user/dto/createUser.dto';
 import { UserService } from '@/user/user.service';
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @UsePipes(new ValidationPipe())
   @Post()
   ///createUser controller class method
   createUser(@Body('user') createUserDto: CreateUserDto): any {
